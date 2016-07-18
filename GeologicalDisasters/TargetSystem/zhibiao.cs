4b8458112e3b7e22666856b88b971952878d6348
AgentWord <434485356@qq.com>
2016-07-18 12:07:12 +0800

    更换牡蛎

---
+++ GeologicalDisasters/TargetSystem/zhibiao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComprehensiveEvaluation.TargetSystem
+{
+    class zhibiao
+    {
+        public static String A_landuse = "";
+        public static String A_disaster = "";
+        public static String A_ecology = "";
+        public static String B_traffic = "";
+        public static String B_DEM = "";
+        public static String  = "";
+        public static String landuse = "";
+    }
+    
+}
